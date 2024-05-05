fe428755d44d81ce733ca77ab8eb60a5311182e5
saetgit <etemadian.sara.1998@gmail.com>
2024-05-05 12:15:00 +0330

    create login

---
+++ pages/index.vue
@@ -0,0 +1,20 @@
+import type { AuthCard } from '#build/components';
+<template>
+    <div>
+        <div class="container">
+            <AuthCard />
+        </div>
+    </div>
+</template>
+
+<style scoped>
+.container {
+    max-width: 50%;
+    margin: 0 auto;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+
+}
+</style>
