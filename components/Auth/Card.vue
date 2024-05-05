d191a32e97361e70e9d68d7c149c409c6740629d
saetgit <etemadian.sara.1998@gmail.com>
2024-05-05 12:24:00 +0330

    Conditionally Displaying Signup or Signin Form

--- components/Auth/Card.vue
+++ components/Auth/Card.vue
@@ -1,12 +1,21 @@
+<script setup lang="ts">
+const authState=ref<"login"|"signup">("login")
+
+const toggleAuthState=()=>{
+    if(authState.value==="login")authState.value="signup";
+    else authState.value="login"
+}
+</script>
 <template>
     <div>
         <NCard class="card">
-            <h3>Login</h3>
+            <h3>{{authState}}</h3>
             <div class="input-container">
                 <input placeholder="Email" />
                 <input placeholder="password" />
             </div>
-            <NButton>Login</NButton>
+            <NButton>Submit</NButton>
+            <p @click="toggleAuthState">{{toggleAuthState==="login"?"Dont have an account? Create one now":"Already have an Account?go ahead a login"}}</p>
         </NCard>
     </div>
 </template>
@@ -19,6 +28,7 @@
 
 .card h3 {
     font-size: 1.5rem;
+    text-decoration: capitalize
 }
 
 .input-container {
@@ -31,6 +41,11 @@
     padding: 0.2rem;
     outline: none;
     border: 0.1rem solid rgba(0,0,0,0.1);
-    border-radius: 1rem
+    border-radius: 0.2rem
+}
+p{
+    color: blue;
+    font-size: 0.5rem;
+    cursor: pointer;
 }
 </style>
