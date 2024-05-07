ecf50fbb9221a5e886ac50b520af5742b012afba
saetgit <etemadian.sara.1998@gmail.com>
2024-05-07 15:55:54 +0330

    add signin

--- components/Auth/Card.vue
+++ components/Auth/Card.vue
@@ -1,61 +1,77 @@
 <script setup lang="ts">
-const authState = ref<"login" | "signup">("login")
+const authState = ref<"login" | "signup">("login");
+const input = reactive({
+  password: "",
+  email: "",
+});
 
-const { signUp } = useAuth();
+const { signUp, signIn, user } = useAuth();
+
 const toggleAuthState = () => {
-    if (authState.value === "login") authState.value = "signup";
-    else authState.value = "login"
-}
+  if (authState.value === "login") authState.value = "signup";
+  else authState.value = "login";
+};
 
-const handelSubmit = () => {
-    signUp({ email: "blumbercourse@gmail.com", password: "password" })
-}
+const handleSubmit = () => {
+  if (authState.value === "login") {
+    // SIGN
+    signIn({ email: input.email, password: input.password });
+  } else {
+    signUp({ email: input.email, password: input.password });
+  }
+};
 </script>
+
 <template>
-    <div>
-        <NCard class="card">
-            <h3>{{ authState }}</h3>
-            <div class="input-container">
-                <input placeholder="Email" />
-                <input placeholder="password" />
-            </div>
-            <NButton @click="handelSubmit">Submit</NButton>
-            <p @click="toggleAuthState">{{
-                authState === "login"
-                    ? "Don't have an account? Create one now"
-                    : "Already have an account? Go ahead an log in"
-                }}</p>
-        </NCard>
-    </div>
+  <div>
+    <NCard class="card">
+      <h3>{{ authState }}</h3>
+      {{ user }}
+      <div class="input-container">
+        {{ input.password }}
+        <input placeholder="Email" v-model="input.email" />
+        <input placeholder="Password" v-model="input.password" />
+      </div>
+      <NButton @click="handleSubmit">Submit</NButton>
+      <p @click="toggleAuthState">
+        {{
+          authState === "login"
+            ? "Don't have an account? Create one now"
+            : "Already have an account? Go ahead an log in"
+        }}
+      </p>
+    </NCard>
+  </div>
 </template>
 
 <style scoped>
 .card {
-    padding: 1rem;
-    width: 25rem;
+  padding: 1rem;
+  width: 25rem;
 }
 
 .card h3 {
-    font-size: 1.5rem;
-    text-decoration: capitalize
+  font-size: 1.75rem;
+  text-transform: capitalize;
 }
 
 .input-container {
-    display: flex;
-    flex-direction: column;
+  display: flex;
+  flex-direction: column;
 }
 
 .input-container input {
-    margin-bottom: 0.3rem;
-    padding: 0.2rem;
-    outline: none;
-    border: 0.1rem solid rgba(0, 0, 0, 0.1);
-    border-radius: 0.2rem
+  margin-bottom: 0.3rem;
+  padding: 0.2rem;
+  outline: none;
+
+  border: 0.1rem solid rgba(0, 0, 0, 0.1);
+  border-radius: 0.2rem;
 }
 
 p {
-    color: blue;
-    font-size: 0.5rem;
-    cursor: pointer;
+  color: blue;
+  font-size: 0.5rem;
+  cursor: pointer;
 }
 </style>
--- composables/useAuth.ts
+++ composables/useAuth.ts
@@ -1,29 +1,39 @@
 const useAuth = () => {
-  const user = useState("user", () => null);
-  const { supabase } = useSupabase();
-
-  supabase.auth.onAuthStateChange((e, session) => {
-    user.value = session?.user || null;
-  });
-
-  const signUp = async ({ email, password, ...metadata }) => {
-    const { user: u, error } = await supabase.auth.signUp(
-      {
+    const user = useState("user", () => null);
+    const { supabase } = useSupabase();
+  
+    supabase.auth.onAuthStateChange((e, session) => {
+      user.value = session?.user || null;
+    });
+  
+    const signUp = async ({ email, password, ...metadata }) => {
+      const { user: u, error } = await supabase.auth.signUp(
+        {
+          email,
+          password,
+        },
+        {
+          data: metadata,
+        }
+      );
+      if (error) throw error;
+      return u;
+    };
+  
+    const signIn = async ({ email, password }) => {
+      const { user: u, error } = await supabase.auth.signIn({
         email,
         password,
-      },
-      {
-        data: metadata,
-      }
-    );
-    if (error) throw error;
-    return u;
+      });
+      if (error) throw error;
+      return u;
+    };
+  
+    return {
+      user,
+      signUp,
+      signIn,
+    };
   };
-
-  return {
-    user,
-    signUp,
-  };
-};
-
-export default useAuth;
+  
+  export default useAuth;
